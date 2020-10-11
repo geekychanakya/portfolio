665d6b7e6f20aa8cf6b62bf78f863968a7bb48f9
geekyChanakya <your-email@bangthetable.com>
2020-10-11 10:50:31 +0530

    Updates

---
+++ precache-manifest.0cf8332eb44b3e20c7a7bc53d666adba.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcf2f7d45edc766b092e9280249afbda",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bab7f9ace9900ac484ea",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "c6d4094f63d5bbd3691e",
+    "url": "/static/css/main.08198a71.chunk.css"
+  },
+  {
+    "revision": "bab7f9ace9900ac484ea",
+    "url": "/static/js/2.eb9e423e.chunk.js"
+  },
+  {
+    "revision": "9668d5d609ed4d6965f47d236623cd24",
+    "url": "/static/js/2.eb9e423e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6d4094f63d5bbd3691e",
+    "url": "/static/js/main.e1f1cc0f.chunk.js"
+  },
+  {
+    "revision": "a1265d8aafc21d0a9c4e",
+    "url": "/static/js/runtime-main.457a83d2.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "067f611865aac45c3b7183a48121ee43",
+    "url": "/static/media/profile_image.067f6118.jpg"
+  }
+]);
